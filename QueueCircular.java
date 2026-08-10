4f286102848606523d87153a1cd2699f27c10ba7
git config --global LakshanEdirisinghe <Ayomall30@gmail.com>
2026-08-10 17:37:38 +0530

    feat(QueueCircular): implement circular queue with full condition handling and enhanced item management

---
+++ QueueCircular.java
@@ -0,0 +1,84 @@
+public class QueueCircular {
+
+    private int arr[];
+    private int frontPoint;
+    private int rearPoint;
+    private int maxSize;
+    private int nItems;
+
+    QueueCircular(int size) {
+        maxSize = size;
+        arr = new int[maxSize];
+        frontPoint = -1;
+        rearPoint = -1;
+        nItems = 0;
+    }
+
+    void Insert(int value) {
+
+        if (rearPoint == maxSize - 1) {
+            rearPoint = -1;
+
+        }
+        arr[++rearPoint] = value;
+        nItems++;
+
+    }
+
+    void Remove() {
+        if (isEmpty()) {
+            System.out.println("Queue is already empty");
+            return;
+        }
+
+        frontPoint++;
+        nItems--;
+
+        if (frontPoint == maxSize) {
+            frontPoint = 0;
+        }
+    }
+
+    void Peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return;
+        }
+
+        System.out.println("Front data: " + arr[frontPoint]);
+    }
+
+    void Display() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty");
+            return;
+        }
+
+        System.out.print("[");
+
+        for (int i = 0; i < nItems; i++) {
+            int index = frontPoint + i;
+
+            if (index >= maxSize) {
+                index -= maxSize;
+            }
+
+            System.out.print(arr[index]);
+
+            if (i < nItems - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println("]");
+    }
+
+    boolean isEmpty() {
+        return (nItems == 0);
+    }
+
+    boolean isFull() {
+        return (nItems == maxSize);
+    }
+
+}
